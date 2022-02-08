ded29da2b97028ad53a2a113f0646cb3d10b69a9
luguofei <lugfa@yonyou.com>
2022-02-08 17:39:02 +0800

    ceshi

--- src/views/bxgl/ywjb/capitalApplication/capitalApplictionAPI.js
+++ src/views/bxgl/ywjb/capitalApplication/capitalApplictionAPI.js
@@ -4,19 +4,19 @@ import myAxios from '../../../../service/http'
 import qs from 'qs'
 export const capitalApplication = {
   // 获取头部carousel数据
-   getUserCommonBillType(){
+  getUserCommonBillType() {
     return myAxios.post('/sysset/arSysCommonBillType/userCommonBillType')
   },
   // 获取tab页签
-  getArBusinssTabs(){
+  getArBusinssTabs() {
     return myAxios.get('/sysset/arsysdatasource/getSourceInfoByTabName/AR_BUSINESS_TABS')
   },
   // 获取表格列
-  getTableColumn(obj){
+  getTableColumn(obj) {
     return myAxios.get(`/sysset/arsystabcol/getTabAndCol/${obj.sourceCode}`)
   },
   // 获取table数据
-  getTableData(param){
-    return myAxios.post('/bill/arBillReport/getArBillReport',param)
+  getTableData(param) {
+    return myAxios.post('/bill/arBillReport/getArBillReport', param)
   }
 }
